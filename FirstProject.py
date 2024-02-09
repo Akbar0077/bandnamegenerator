96fa4f9cab308970a967ecfbd2a887fed8d95d52
Akbar0077 <148612146+Akbar0077@users.noreply.github.com>
2024-02-09 10:47:58 +0530

    Add files via upload

---
+++ FirstProject.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator.")
+Name = input ("What's name is to prefer?\n")
+Nick = input ("What's your nickname?\n") 
+print("Your band name could be " + Nick + " " + Name)
